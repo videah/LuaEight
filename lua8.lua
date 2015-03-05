5f6d2be23edf99fea7d547f341be80c30ee0b84d
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-03-05 23:38:55 +0000

    May rewrite all this...
    ew

--- lua8.lua
+++ lua8.lua
@@ -3,7 +3,8 @@ local eight = {}
 eight.memory = {}
 eight.stack = {}
 eight.display_buffer = {}
-eight.pc = 512
+eight.REG = {}
+eight.pc = 512 -- Start counter after reserved memory space
 eight.index = 0
 eight.timer_delay = 0
 eight.sound_delay = 0
@@ -27,10 +28,6 @@ eight.fontset = {
 0xF0, 0x80, 0xF0, 0x80, 0xF0,
 0xF0, 0x80, 0xF0, 0x80, 0x80}
 
-eight.funcmap = {
-
-}
-
 -- Memory --
 for i=1, 4096 do
 	eight.memory[i] = 0
@@ -48,6 +45,134 @@ for x = 0, 64 do
 		eight.display_buffer[x][y] = math.random(0,1)
 	end
 end
+
+function instruction_1(opcode, base, address)
+
+	if address == 0x0E0 then
+		for x = 0, 64 do
+			eight.display_buffer[x] = {}
+			for y = 0, 32 do
+				eight.display_buffer[x][y] = 0
+			end
+		end
+	elseif address == 0x0EE then
+		eight.pc = eight.stack[#eight.stack]
+		eight.stack[#eight.stack] = nil
+	else
+		print("RCA not implemented.")
+		print(eight.pc)
+	end
+end
+
+function instruction_2(opcode, base, address, pX, pY, value, subbase)
+
+	eight.pc = address
+
+end
+
+function instruction_3(opcode, base, address, pX, pY, value, subbase)
+
+	eight.stack[#eight.stack + 1] = eight.pc
+	eight.pc = address
+
+end
+
+function instruction_4(opcode, base, address, pX, pY, value, subbase)
+	if eight.REG[pX] == value then
+		eight.pc = eight.pc + 2
+	end
+end
+
+function instruction_5(opcode, base, address, pX, pY, value, subbase)
+	if eight.REG[pX] ~= value then
+		eight.pc = eight.pc + 2
+	end
+end
+
+function instruction_6(opcode, base, address, pX, pY, value, subbase)
+
+	if eight.REG[pX] == eight.REG[pY] then
+		eight.pc = eight.pc + 2
+	end
+
+end
+
+function instruction_7(opcode, base, address, pX, pY, value, subbase)
+
+	eight.REG[pX] = value
+
+end
+
+function instruction_8(opcode, base, address, pX, pY, value, subbase)
+
+	eight.REG[pX] = (eight.REG[pX] + value) % 256
+
+end
+
+function instruction_9(opcode, base, address, pX, pY, value, subbase)
+
+	if subbase == 14 or (subbase >= 0 and subbase <=7) then
+
+		if subbase == 0 then
+			eight.REG[pX] = eight.REG[pY]
+		elseif subbase == 1 then 
+			eight.REG[pX] = bit32.bor(eight.REG[pX],eight.REG[pY])
+		elseif subbase == 2 then 
+			eight.REG[pX] = bit32.band(eight.REG[pX],eight.REG[pY])
+		elseif subbase == 3 then 
+			eight.REG[pX] = bit32.bxor(eight.REG[pX],eight.REG[pY])
+		elseif subbase == 4 then 
+			local tmp = eight.REG[pX] + eight.REG[pY]
+			eight.REG[15] = tmp > 255 and 1 or 0
+			eight.REG[pX] = tmp % 256
+		elseif subbase == 5 then 
+			local tmp = eight.REG[pX] - eight.REG[pY]
+			eight.REG[15] = tmp >= 0 and 1 or 0
+			eight.REG[pX] = (tmp + 256) % 256
+		elseif subbase == 6 then 
+			eight.REG[15] = eight.REG[pY] % 2
+			eight.REG[pX] = math.floor(eight.REG[pY] / 2)
+		elseif subbase == 7 then 
+			local tmp = eight.REG[pY] - eight.REG[pX]
+			eight.REG[15] = tmp >= 0 and 1 or 0
+			eight.REG[pX] = (tmp + 256) % 256
+		elseif subbase == 14 then
+			eight.REG[15] = math.floor(eight.REG[pY] / 128) % 2
+			eight.REG[pX] = (eight.REG[pY] * 2) % 256
+		end
+	end
+
+end
+
+function instruction_9(opcode, base, address, pX, pY, value, subbase)
+
+	if subbase == 0 then
+		if eight.REG[pX] ~= eight.REG[pY] then
+			eight.pc = eight.pc + 2
+		end
+	end
+
+end
+
+function instruction_10(opcode, base, address, pX, pY, value, subbase)
+
+	eight.index = address
+
+end
+
+eight.funcmap = {
+instruction_1,
+instruction_2,
+instruction_3,
+instruction_4,
+instruction_5,
+instruction_6,
+instruction_7,
+instruction_8,
+instruction_9,
+instruction_10
+
+}
 
 function eight.loadROM(rom)
 
@@ -78,13 +203,17 @@ function eight.cycle()
 
 		eight.pc = (eight.pc + 2) % 4096
 
-		local base = math.floor(opcode/4096)
+		local base = math.floor(opcode/4096) + 1
 		local address = opcode % 4096
+		local pX = math.floor(opcode/256)%16
+		local pY = math.floor(opcode/16)%16
+		local subbase = opcode%16
+		local value = opcode%256
 
-		if pcall(function() eight.funcmap[base]() end) == false then
+		if pcall(function() eight.funcmap[base](opcode, base, address, pX, pY, value, subbase) end) == false then
 			print("Unknown instruction: " .. base)
 		else
-			print("YEY")
+			print("Successfuly ran instruction: " .. base)
 		end
 
 	end
